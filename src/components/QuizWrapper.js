41057412c0dc7787ee3198d643db63fe6c03fb97
denny <denny.webdev@gmail.com>
2018-09-02 15:22:42 -0400

    adjust card width

--- src/components/QuizWrapper.js
+++ src/components/QuizWrapper.js
@@ -3,7 +3,7 @@ import { Card } from '@hackclub/design-system'
 
 export default function QuizWrapper (props) {
   return (
-    <Card bg='white' boxShadowSize='sm' ml='auto' mr='auto' my={4} p={3} width={500} align='center'>
+    <Card bg='white' boxShadowSize='sm' ml='auto' mr='auto' my={4} p={3} width={0.9} align='center'>
       {props.children}
     </Card>
   )
